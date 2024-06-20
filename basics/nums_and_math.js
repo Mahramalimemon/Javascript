79f6f4f12fda892f6e488ee38d6f3e4f70df9688
Mahramalimemon <140167482+Mahramalimemon@users.noreply.github.com>
2024-06-20 11:57:57 +0000

    nums and math

---
+++ basics/nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 400;                      //number value
+console.log(score);
+console.log(score.toString().length);
+console.log(score.toFixed(4));
+
+const balance = new Number(250);        //number value
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+//      ++++++++++++++ Maths ++++++++++++++
+
+console.log(Math.abs(-10));
+console.log(Math.round(5.556));
+console.log(Math.ceil(7.7));
+console.log(Math.floor(6.9));
