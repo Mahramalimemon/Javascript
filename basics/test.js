239d894694d9573634f1bb9b8abc3057f544f207
Mahramalimemon <140167482+Mahramalimemon@users.noreply.github.com>
2024-06-20 09:15:20 +0000

    string methods

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,11 @@
 console.log("Mahram");
+let myName = 'Mahram Ali';                  //string value
+let hisName = new String("Noshad");         //string value
+
+console.log(myName);
+console.log(myName.toUpperCase());
+console.log(myName[2]);
+console.log(myName.indexOf("r"));
+
+console.log(hisName);
+console.log(hisName.toUpperCase());
